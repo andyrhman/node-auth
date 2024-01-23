aabcb5087da40c76b48c9d0df0c594fa03ce9f7f
andyrhman <andyrahman989@gmail.com>
2024-01-23 21:14:04 +0800

    Added reset password

--- src/config/transporter.ts
+++ src/config/transporter.ts
@@ -1,14 +1,14 @@
 import { createTransport } from "nodemailer";
 
 export const transporter = createTransport({
-    // service: "Gmail",
-    // host: "smtp.gmail.com",
-    // port: 465,
-    // secure: true,
-    // auth: {
-    //     user: process.env.GMAIL_EMAIL,
-    //     pass: process.env.GMAIL_PASSWORD,
-    // },
-    host: '0.0.0.0',
-    port: 1025
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASSWORD,
+    },
+    // host: '0.0.0.0',
+    // port: 1025
 });
--- src/controller/forgot.controller.ts
+++ src/controller/forgot.controller.ts
@@ -4,10 +4,12 @@ import { transporter } from "../config/transporter";
 import * as crypto from 'crypto';
 import * as fs from 'fs';
 import * as handlebars from 'handlebars';
+import * as argon2 from 'argon2'
 import myDataSource from "../config/data-source";
 import logger from "../config/logger";
+import { User } from "../entity/user.entity";
 
-export const Forgot = async (req: Request, res: Response) => {
+export const ForgotPassword = async (req: Request, res: Response) => {
     try {
         const { email } = req.body;
 
@@ -46,5 +48,40 @@ export const Forgot = async (req: Request, res: Response) => {
         logger.error(error.message);
         return res.status(500).send({ message: error.message })
     }
+}
 
+export const ResetPassword = async (req: Request, res: Response) => {
+    try {
+        const { token, password, password_confirm } = req.body;
+
+        if (password !== password_confirm) {
+            return res.status(400).send({ message: "Password do not match" })
+        }
+        // check for token
+        const checkToken = await myDataSource.getRepository(Reset).findOne({ where: { token: token } });
+
+        if (!checkToken || checkToken.expiresAt < Date.now() || checkToken.used) {
+            return res.status(400).send({ message: "Invalid Request" });
+        }
+
+        // find user from the token
+        const user = await myDataSource.getRepository(User).findOne({ where: { email: checkToken.email } });
+
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        await myDataSource.getRepository(User).update(user.id, {
+            password: await argon2.hash(password)
+        });
+
+        await myDataSource.getRepository(Reset).update(checkToken.id, {
+            used: true
+        });
+
+        res.status(202).send({ message: "Password updated successfully" })
+    } catch (error) {
+        logger.error(error.message);
+        return res.status(500).send({ message: error.message })
+    }
 }
